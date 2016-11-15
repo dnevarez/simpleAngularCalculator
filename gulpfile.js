05e777a00e9834b329d8883692c36ec8dbdd8bdf
Dan Nevarez <dnev.dev@gmail.com>
2016-11-14 20:46:09 -0700

    add styling and some fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
-// var babel = require('gulp-babel');
+var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var order = require('gulp-order');
 
-// gulp.task('es6', function () {
-//   return gulp.src([ './js/*.js','./js/**/*.js'])
-//   .pipe(plumber())
-//   .pipe(babel({
-//     "presets": ["es2015"]
-//   }))
-//   .pipe(order([
-//     'app.js', '*.js'
-//   ]))
-//   .pipe(concat('all.js'))
-//   .pipe(gulp.dest('dist'));
-// })
+gulp.task('es6', function () {
+  return gulp.src([ './js/*.js','./js/**/*.js'])
+  .pipe(plumber())
+  .pipe(babel({
+    "presets": ["es2015"]
+  }))
+  .pipe(order([
+    'app.js', '*.js'
+  ]))
+  .pipe(concat('all.js'))
+  .pipe(gulp.dest('dist'));
+})
 
 gulp.task('sass', function() {
-  return gulp.src('*.scss')
+  return gulp.src('./styles/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(concat('all.css'))
   .pipe(gulp.dest('dist'))
@@ -27,6 +27,8 @@ gulp.task('sass', function() {
 
 gulp.task('watch', function() {
   gulp.watch('./styles/*.scss', ['sass'])
+  // gulp.watch('./styles/**/*.scss', ['sass'])
+  gulp.watch('./js/*.js', ['es6'])
 });
 
-gulp.task('default', ['watch', 'sass']);
+gulp.task('default', ['watch', 'es6', 'sass'])
---
+++ js/mainCtrl.js
@@ -0,0 +1,218 @@
+angular.module('app')
+.controller('calcCtrl', function($scope){
+
+///////////////////////////////////
+// Array of data for the buttons //
+///////////////////////////////////
+
+// Number buttons and a few others.
+  $scope.buttonsLeft = [
+    {value: 'AC', id: 'clearall'},
+    {value: '+/-', id: 'plusMinus'},
+    {value: '%', id: 'percent'},
+    {value: '1', id: '1'},
+    {value: '2', id: '2'},
+    {value: '3', id: '3'},
+    {value: '4', id: '4'},
+    {value: '5', id: '5'},
+    {value: '6', id: '6'},
+    {value: '7', id: '7'},
+    {value: '8', id: '8'},
+    {value: '9', id: '9'},
+    {value: '0', id: 'zero'},
+    {value: '.', id: 'dot'}
+  ]
+
+// Operator buttons
+$scope.operators = [
+  {value: '/', id:'divide'},
+  {value: '*', id:'multi'},
+  {value: '-', id:'minus'},
+  {value: '+', id:'plus'},
+  {value: '=', id: 'equals'}
+]
+
+///////////////
+// Variables //
+///////////////
+
+// Variables to store number inputs and operator.
+  var number = '',
+      newnumber = '',
+      operator = '';
+
+// Input the user will see in the total div.
+  $scope.totaldiv = '0';
+
+///////////////
+// Functions //
+///////////////
+
+// Functions to limit total length to nine characters in the total display.
+var myNamespace = {};
+
+myNamespace.round = function(number, precision) {
+    var factor = Math.pow(10, precision);
+    var tempNumber = number * factor;
+    var roundedTempNumber = Math.round(tempNumber);
+    return roundedTempNumber / factor;
+};
+
+function roundDec(c) {
+  if(Math.round(c).toString().length === 9) {
+    $scope.totaldiv =	Math.round(c);
+  } else if (Math.round(c).toString().length > 9) {
+      number = "";
+      $scope.totaldiv = "Err: Number too long for output"
+    } else {
+        var x = 8 - Math.round(c).toString().length;
+        $scope.totaldiv =	myNamespace.round(c, x).toString();
+        console.log($scope.totaldiv);
+      }
+}
+
+// Tests to see if input is too long, so it doesn't break out of container.
+var testNumLength = function(num) {
+  console.log(num, num.length);
+  if (num.length > 9) {
+        roundDec(num);
+      } else {
+        $scope.totaldiv = num
+      }
+  };
+
+// Clears last value stored.
+  var clear = function() {
+    if (number.length === 0) $scope.operation = '';
+    number = '';
+    $scope.totaldiv = '0';
+    $scope.buttonsLeft[0] = {value: 'AC', id: 'clearall'}
+  }
+
+// Clears both stored values.
+  var clearall = function(){
+    number = '';
+    newnumber = ''
+    $scope.operation = ''
+    $scope.totaldiv = '0';
+  }
+
+// Changes value to either negative or positive.
+  var plusMinus = function(){
+    // Function checks which input is currently displayed in order to properly
+    // assign the positive/negative attribute to the correct stored value.
+    if(number === $scope.totaldiv) number = (parseFloat($scope.totaldiv) * -1).toString();
+    if(newnumber === $scope.totaldiv) newnumber = (parseFloat($scope.totaldiv) * -1).toString();
+
+    // Assigns pos/neg attribute to what the user sees.
+    $scope.totaldiv = (parseFloat($scope.totaldiv) * -1).toString();
+  }
+
+// Totals inputs
+var totalEquals = function() {
+  if (number.length === 0 && newnumber.length === 0) {
+    $scope.totaldiv = '0';
+  }
+  else {
+    // Parse numbers to do Math.
+    number = parseFloat (number)
+    newnumber = parseFloat(newnumber)
+    // Total container for answer.
+    var total = 0;
+    // Check operator.
+    if(operator === '+') {
+        total = newnumber + number;
+    } else if (operator === '-') {
+        total = newnumber - number;
+    } else if (operator === '/') {
+        total = newnumber / number;
+    } else if (operator === '*') {
+        total = newnumber * number;
+    }
+
+    // Tests for length of total and sets totaldiv value for user.
+    testNumLength(total.toString());
+
+    // Stores total and resets newnumber for chained operations.
+    number = total.toString();
+    newnumber = '';
+  }
+}
+
+var percent = function() {
+  if(number.length > 0){
+    $scope.operation = '%';
+    // Variable to store total.
+    var percentTotal;
+    // Parse number to use in equation.
+    number = parseFloat(number);
+
+    // Check if newnumber has no stored value to avoid parse error (NaN)
+    if (newnumber.length === 0){
+      percentTotal = number / 100;
+    }
+    else {
+    // Otherwise parse newnumber as well and find percentage.
+      newnumber = parseFloat(newnumber)
+      percentTotal = (newnumber / 100) * number;
+    }
+    number = percentTotal.toString();
+    testNumLength(number);
+    // Stores total and resets newnumber for chained operations.
+    // number = percentTotal.toString();
+    newnumber = '';
+  }
+}
+
+
+// Finds the total if equals button is pressed, otherwise stores the operator and also sets it to scope for user.
+$scope.storeOperator = function() {
+  $scope.operation = operator;
+
+  if(this.operator.value === '=') {
+    $scope.operation = '=';
+    totalEquals();
+  } else {
+    operator = this.operator.value;
+    $scope.operation = operator;
+    newnumber = number;
+    number = "";
+  }
+}
+
+// This is where all our functions will be invoked.
+$scope.click = function(x) {
+
+  if(this.button.id === 'clear') {
+    clear();
+  } else if (this.button.id === 'clearall') {
+      clearall();
+  } else if (this.button.id === 'plusMinus') {
+      plusMinus();
+  } else if (this.button.value === "%") {
+      percent();
+    } else {
+      $scope.buttonsLeft[0] = {value: 'C', id: 'clear'};
+      number += this.button.value;
+      $scope.totaldiv = number;
+  }
+}
+
+// Color change when buttons are momentarily changed when pressed. 
+
+$scope.mouseDown = function() {
+  if(this.button) {
+    document.getElementById(this.button.id).style.background = '-webkit-gradient(linear, left top, left bottom, from(#CCCCCC), to(#C2C2C2))';
+  } else {
+    document.getElementById(this.operator.id).style.background = '-webkit-gradient(linear, left top, left bottom, from(#CCCCCC), to(#C2C2C2))';
+  }
+}
+$scope.mouseUp = function() {
+  if(this.button) {
+    document.getElementById(this.button.id).style.background = '-webkit-gradient(linear, left top, left bottom, from(#EDEDED), to(#DCDCDC))';
+  } else {
+    document.getElementById(this.operator.id).style.background = '-webkit-gradient(linear, left top, left bottom, from(#EDEDED), to(#DCDCDC))';
+  }
+}
+
+})
